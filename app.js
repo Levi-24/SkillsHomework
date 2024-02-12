27a3b6067413365d5107106775bada7821b29f49
Levente <122294490+Levi-24@users.noreply.github.com>
2024-02-12 12:13:22 +0100

    changes

--- app.js
+++ app.js
@@ -5,4 +5,22 @@ app.controller("CalculatorController", function($scope, $http){
         $scope.stages = response.data;
         console.log($scope.stages);
     })
-})
+
+    $scope.teamMembers = [];
+for (let i = 0; i < 10; i++) {
+    $scope.teamMembers.push({
+        firstName: "",
+        lastName: "",
+        speed: "",
+        totalDistance: 0
+    })
+}
+
+$scope.updateRunners = function(){
+    $scope.runners = [];
+}
+
+$scope.formatTime = function(){
+    
+}
+});
